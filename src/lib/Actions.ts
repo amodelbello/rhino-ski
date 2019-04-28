1a6cf32def8738e171f811a70741f174252a15d5
Amo DelBello <amodelbello@gmail.com>
2019-04-27 21:58:19 -0600

    Substantially refactor Actions.ts class

--- src/lib/Actions.ts
+++ src/lib/Actions.ts
@@ -10,32 +10,26 @@ export default class Actions {
 
   public moveUp() {
     if (this.heroCanMove()) {
-      this.updateHeroDirection(GameAction.MoveUp);
-      // Need to manually call this because
-      // hero will be stopped before next time through the loop
-      this.game.nextFrame();
-      this.game.hero.isMoving = false;
+      this.updateHero(GameAction.MoveUp);
     }
   }
 
   public moveLeft() {
     if (this.heroCanMove()) {
-      this.game.hero.isMoving = true;
-      this.updateHeroDirection(GameAction.MoveLeft);
+      this.updateHero(GameAction.MoveLeft);
+    }
+  }
+
+  public moveRight() {
+    if (this.heroCanMove()) {
+      this.updateHero(GameAction.MoveRight);
     }
   }
 
   public moveDown() {
     if (this.heroCanMove()) {
       this.game.hero.isMoving = true;
-      this.updateHeroDirection(GameAction.MoveDown);
-    }
-  }
-
-  public moveRight() {
-    if (this.heroCanMove()) {
-      this.game.hero.isMoving = true;
-      this.updateHeroDirection(GameAction.MoveRight);
+      this.updateHero(GameAction.MoveDown);
     }
   }
 
@@ -52,84 +46,115 @@ export default class Actions {
     return false;
   }
 
-  private updateHeroDirection(action: GameAction) {
-    // TODO: Yowsers! this is hard to read. Maybe go back to the original way
+  private updateHero(action: GameAction) {
+    // TODO: Yowsers! this is hard to read. Can this be improved?
     switch (action) {
       case GameAction.MoveUp:
         if (!this.game.hero.isMoving) {
-          this.hike();
+          this.hike(Direction.North);
         }
-        if (
-          this.game.hero.direction === Direction.Crash ||
-          this.game.hero.direction === Direction.East ||
-          this.game.hero.direction === Direction.SouthEast
-        ) {
-          this.game.hero.direction = Direction.East;
-          this.game.hero.image = this.game.images.skierRight;
-        } else if (
-          this.game.hero.direction === Direction.West ||
-          this.game.hero.direction === Direction.SouthWest
-        ) {
-          this.game.hero.direction = Direction.West;
-          this.game.hero.image = this.game.images.skierLeft;
-        } else if (this.game.hero.direction === Direction.South) {
-          this.game.hero.direction = Direction.SouthWest;
-          this.game.hero.image = this.game.images.skierRight;
+        switch (this.game.hero.direction) {
+          case Direction.Crash:
+          case Direction.East:
+          case Direction.SouthEast:
+            this.game.hero.direction = Direction.East;
+            this.game.hero.image = this.game.images.skierRight;
+            break;
+          case Direction.West:
+          case Direction.SouthWest:
+            this.game.hero.direction = Direction.West;
+            this.game.hero.image = this.game.images.skierLeft;
+            break;
+          case Direction.South:
+            this.game.hero.direction = Direction.SouthWest;
+            this.game.hero.image = this.game.images.skierRight;
+            break;
         }
         break;
+
       case GameAction.MoveLeft:
-        if (
-          this.game.hero.direction === Direction.Crash ||
-          this.game.hero.direction === Direction.West ||
-          this.game.hero.direction === Direction.SouthWest
-        ) {
-          this.game.hero.direction = Direction.West;
-          this.game.hero.image = this.game.images.skierLeft;
-        } else if (this.game.hero.direction === Direction.South) {
-          this.game.hero.direction = Direction.SouthWest;
-          this.game.hero.image = this.game.images.skierLeftDown;
-        } else if (this.game.hero.direction === Direction.SouthEast) {
-          this.game.hero.direction = Direction.South;
-          this.game.hero.image = this.game.images.skierDown;
-        } else if (this.game.hero.direction === Direction.East) {
-          this.game.hero.direction = Direction.SouthEast;
-          this.game.hero.image = this.game.images.skierRightDown;
+        switch (this.game.hero.direction) {
+          case Direction.Crash:
+            this.game.hero.direction = Direction.West;
+            this.game.hero.image = this.game.images.skierLeft;
+            this.hike(Direction.West);
+            break;
+          case Direction.West:
+            this.game.hero.direction = Direction.West;
+            this.game.hero.image = this.game.images.skierLeft;
+            this.hike(Direction.West);
+            break;
+          case Direction.SouthWest:
+            this.game.hero.direction = Direction.West;
+            this.game.hero.image = this.game.images.skierLeft;
+            break;
+          case Direction.South:
+            this.game.hero.direction = Direction.SouthWest;
+            this.game.hero.image = this.game.images.skierLeftDown;
+            break;
+          case Direction.SouthEast:
+            this.game.hero.direction = Direction.South;
+            this.game.hero.image = this.game.images.skierDown;
+            break;
+          case Direction.East:
+            this.game.hero.direction = Direction.SouthEast;
+            this.game.hero.image = this.game.images.skierRightDown;
+            break;
         }
         break;
-      case GameAction.MoveDown:
-        if (
-          this.game.hero.direction === Direction.Crash ||
-          this.game.hero.direction === Direction.SouthEast ||
-          this.game.hero.direction === Direction.SouthWest ||
-          this.game.hero.direction === Direction.South
-        ) {
-          this.game.hero.direction = Direction.South;
-          this.game.hero.image = this.game.images.skierDown;
-        } else if (this.game.hero.direction === Direction.West) {
-          this.game.hero.direction = Direction.SouthWest;
-          this.game.hero.image = this.game.images.skierLeftDown;
-        } else if (this.game.hero.direction === Direction.East) {
-          this.game.hero.direction = Direction.SouthEast;
-          this.game.hero.image = this.game.images.skierRightDown;
-        }
-        break;
+
       case GameAction.MoveRight:
-        if (
-          this.game.hero.direction === Direction.Crash ||
-          this.game.hero.direction === Direction.East ||
-          this.game.hero.direction === Direction.SouthEast
-        ) {
-          this.game.hero.direction = Direction.East;
-          this.game.hero.image = this.game.images.skierRight;
-        } else if (this.game.hero.direction === Direction.South) {
-          this.game.hero.direction = Direction.SouthEast;
-          this.game.hero.image = this.game.images.skierRightDown;
-        } else if (this.game.hero.direction === Direction.SouthWest) {
-          this.game.hero.direction = Direction.South;
-          this.game.hero.image = this.game.images.skierDown;
-        } else if (this.game.hero.direction === Direction.West) {
-          this.game.hero.direction = Direction.SouthWest;
-          this.game.hero.image = this.game.images.skierLeftDown;
+        switch (this.game.hero.direction) {
+          case Direction.Crash:
+            this.game.hero.direction = Direction.East;
+            this.game.hero.image = this.game.images.skierRight;
+            this.hike(Direction.East);
+            break;
+          case Direction.East:
+            this.game.hero.direction = Direction.East;
+            this.game.hero.image = this.game.images.skierRight;
+            this.hike(Direction.East);
+            break;
+          case Direction.SouthEast:
+            this.game.hero.direction = Direction.East;
+            this.game.hero.image = this.game.images.skierRight;
+            break;
+          case Direction.South:
+            this.game.hero.direction = Direction.SouthEast;
+            this.game.hero.image = this.game.images.skierRightDown;
+            break;
+          case Direction.SouthWest:
+            this.game.hero.direction = Direction.South;
+            this.game.hero.image = this.game.images.skierDown;
+            break;
+          case Direction.West:
+            this.game.hero.direction = Direction.SouthWest;
+            this.game.hero.image = this.game.images.skierLeftDown;
+            break;
+        }
+        break;
+
+      case GameAction.MoveDown:
+        switch (this.game.hero.direction) {
+          case Direction.Crash:
+            this.game.hero.direction = Direction.South;
+            this.game.hero.image = this.game.images.skierDown;
+            this.hike(Direction.South);
+            break;
+          case Direction.South:
+          case Direction.SouthEast:
+          case Direction.SouthWest:
+            this.game.hero.direction = Direction.South;
+            this.game.hero.image = this.game.images.skierDown;
+            break;
+          case Direction.West:
+            this.game.hero.direction = Direction.SouthWest;
+            this.game.hero.image = this.game.images.skierLeftDown;
+            break;
+          case Direction.East:
+            this.game.hero.direction = Direction.SouthEast;
+            this.game.hero.image = this.game.images.skierRightDown;
+            break;
         }
         break;
       default:
@@ -137,10 +162,10 @@ export default class Actions {
     }
   }
 
-  private hike() {
+  private hike(direction: Direction) {
     const currentDirection = this.game.hero.direction;
     this.game.hero.isMoving = true;
-    this.game.hero.direction = Direction.North;
+    this.game.hero.direction = direction;
     this.game.moveExistingObstacles(2);
     this.game.nextFrame();
     this.game.hero.direction = currentDirection;
